93708ef6ff43aff8b2eadc4b42cbe352fc5215f8
HITRBA <87818867+NoMoreSweet@users.noreply.github.com>
2023-05-17 03:10:12 +0300

    Update bot.py
    Показывает влажность+температуру и скорость ветра

--- ботт/bot.py
+++ ботт/bot.py
@@ -1,20 +1,27 @@
+import os
+import math
 import telebot
 from telebot import types
 import config
 import requests, json
+
+bot = telebot.TeleBot(config.TELEGRAM_API_KEY)
 # прописать в терманале: pip install telebot
 def weather(city, API_KEY):
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
     URL = BASE_URL + "q=" + city + "&appid=" + API_KEY
-    response = requests.get(URL)
+    response = requests.get(URL,
+                            params={'units': 'metric', 'lang': 'ru'})
     if response.status_code == 200:
         data = response.json()
-        temperature = int(data['main']['temp'] - 273.15)
-        return temperature
+        weather_description = data["weather"][0]["main"]
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        return temperature, humidity, pressure, wind
     else:
-        print("Неудачный запрос((")
-        
-bot = telebot.TeleBot(config.TELEGRAM_API_KEY)
+        print()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -24,7 +31,10 @@ def start_message(message):
 def print_weather(message):
     city = message.text
     temperature = weather(city, config.WEATHER_API_KEY)
-    msg = bot.reply_to(message, f"Текущая температура: {temperature}°C")
+    humidity = weather(city, config.WEATHER_API_KEY)
+    pressure = weather(city, config.WEATHER_API_KEY)
+    wind = weather(city, config.WEATHER_API_KEY)
+    msg = bot.reply_to(message, f"Текущая температура: {temperature[0]}°C\nВлажность: {humidity[1]}%\nДавление: {pressure[2]} мм.рт.ст\nВетер: {wind[3]} м/с")
     bot.send_message(message.chat.id, "Введите название города, чтобы продолжить")
     bot.register_next_step_handler(msg, print_weather)
 bot.polling()
